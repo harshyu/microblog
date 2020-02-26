3b8618fffe7ef7cba577f72381383956e64206f2
twiliharsh <harshd403@gmail.com>
2020-02-26 23:01:49 +0530

    third

--- js/main.js
+++ js/main.js
@@ -49,9 +49,7 @@ $(document).ready(function () {
                     }
                     if (d.photo) {
                         $("header").css('background-image', "url('" + d.photo + "')");
-                        $ {
-                            "#postImg"
-                        }.attr("src", d.photo);
+                        $("#postImg").attr("src", d.photo);
                     }
 
                 } else {
